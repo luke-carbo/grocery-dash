799dbf2d172d23bbbbe45d356ef62bb7f69f9960
Ian Laptop <imb4@sfu.ca>
2026-03-05 18:29:45 -0800

    Dynamic Constructors -> Main Constructors
    Minor Updates WIP

--- grocerygame/src/main/java/group18/Entity_Dynamic.java
+++ grocerygame/src/main/java/group18/Entity_Dynamic.java
@@ -9,36 +9,4 @@ public abstract class Entity_Dynamic extends Game_Entity{
      */
     private int speed;
 
-    /**
-     * Retrieves the x-coordinate of this entity.
-     *
-     * @return the current x-coordinate.
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * Retrieves the y-coordinate of this entity.
-     * @return the current y-coordinate.
-     */
-    public int getY() {
-        return y;
-    }
-
-    /**
-     * Updates the x-coordinate of this entity.
-     * @param x the new x-coordinate to set for this entity.
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * Updates the y-coordinate of this entity.
-     * @param y the new y-coordinate to set for this entity.
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
 }
--- grocerygame/src/main/java/group18/Game_Entity.java
+++ grocerygame/src/main/java/group18/Game_Entity.java
@@ -1,10 +1,59 @@
 package group18;
 
+/**
+ * Abstract master entity class with constructors and getters for standard entity values.
+ */
 public abstract class Game_Entity {
+    /**
+     * The entity's X-Coordinate on the Map.
+     */
     public int position_x;
+
+    /**
+     * The entity's Y-Coordinate on the Map.
+     */
     public int Position_y;
 
+    /**
+     * Abstract declaration for different collision types and handling
+     */
     public abstract void collision();
 
+    /**
+     * Abstract declaration for different update types and handling
+     */
     public abstract void update();
+
+    /**
+     * Retrieves the X-Coordinate of this entity.
+     *
+     * @return the current X-Coordinate.
+     */
+    public int getX() {
+        return position_x;
+    }
+
+    /**
+     * Retrieves the Y-Coordinate of this entity.
+     * @return the current Y-Coordinate.
+     */
+    public int getY() {
+        return Position_y;
+    }
+
+    /**
+     * Updates the X-Coordinate of this entity.
+     * @param x the new X-Coordinate to set for this entity.
+     */
+    public void setX(int x) {
+        this.position_x = x;
+    }
+
+    /**
+     * Updates the Y-Coordinate of this entity.
+     * @param y the new Y-Coordinate to set for this entity.
+     */
+    public void setY(int y) {
+        this.Position_y = y;
+    }
 }
