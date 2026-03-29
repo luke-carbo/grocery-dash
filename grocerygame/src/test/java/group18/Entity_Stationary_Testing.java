31e6c2e71f99e2cc819520fef79e9fef98f356d0
Ian Laptop <imb4@sfu.ca>
2026-03-28 20:42:57 -0700

    Test Planning

---
+++ grocerygame/src/test/java/group18/Entity_Stationary_Testing.java
@@ -0,0 +1,20 @@
+package group18;
+
+import org.junit.jupiter.api.Test;
+
+public class Entity_Stationary_Testing {
+
+    @Test
+    // Test01 -
+    void test01() {
+        //
+    }
+
+    // Tests Needed
+    // Spawn Entity Exceeding Limit
+    // Spawn Entity Overlapping Other Entity (Will Fail)
+    // Spawn Entity Overlapping Wall
+    // Spawn Entity In Start / End Area
+    // Spawn Entity On Player
+    // Entity Collection Priority
+}
